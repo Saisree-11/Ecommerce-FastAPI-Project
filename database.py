a2bc79a7677953a9b98b0fb7a9bfe619cb4f9fa2
Saisree-11 <saisree.paladugu29@gmail.com>
2026-07-30 10:44:59 +0530

    update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 # MySQL Database Connection
-DATABASE_URL = "mysql://avnadmin:AVNS_KaQ-79hOzWJIXvNWcRw@mysql-12f443d8-saisree-3c10.j.aivencloud.com:28388/defaultdb"
+DATABASE_URL = "mysql+pymsql://avnadmin:AVNS_KaQ-79hOzWJIXvNWcRw@mysql-12f443d8-saisree-3c10.j.aivencloud.com:28388/defaultdb"
 
 engine = create_engine(DATABASE_URL)
 
