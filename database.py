387ffd7e00afa5d161ed114ba7f5d52908d3f9a1
Saisree-11 <saisree.paladugu29@gmail.com>
2026-07-30 10:49:51 +0530

    update database.py

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-# MySQL Database Connection
-DATABASE_URL = "mysql+pymsql://avnadmin:AVNS_KaQ-79hOzWJIXvNWcRw@mysql-12f443d8-saisree-3c10.j.aivencloud.com:28388/defaultdb"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(DATABASE_URL, connect_args={"ssl": {"ssl_disabled": False}})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
